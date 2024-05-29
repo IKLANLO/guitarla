2071ee2d3a35cf1757bbd9afed92a43684ec6169
IKLANLO <ilandaberea@yahoo.es>
2024-05-29 20:18:29 +0000

    cambios menores

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
             <a href="index.html">
               <img
                 className="img-fluid"
-                src="./public/img/logo.svg"
+                src="/img/logo.svg"
                 alt="imagen logo"
               />
             </a>
@@ -21,7 +21,7 @@ const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
             <div className="carrito">
               <img
                 className="img-fluid"
-                src="./public/img/carrito.png"
+                src="/img/carrito.png"
                 alt="imagen carrito"
               />
 
@@ -45,7 +45,7 @@ const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clear
                         <td>
                           <img
                             className="img-fluid"
-                            src={`./public/img/${guitar.image}.jpg`}
+                            src={`/img/${guitar.image}.jpg`}
                             alt="imagen guitarra"
                           />
                         </td>
